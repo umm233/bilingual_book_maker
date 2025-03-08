dd40f710db8a90b9b0b7e099ab0cc34b45e67ed5
umm233 <umm233@protonmail.com>
2025-03-08 22:56:53 +0800

    feat: add more groq support model

--- book_maker/translator/groq_translator.py
+++ book_maker/translator/groq_translator.py
@@ -5,10 +5,21 @@ from itertools import cycle
 
 
 GROQ_MODEL_LIST = [
+    "gemma2-9b-it",
+    "llama-3.3-70b-versatile",
+    "llama-3.1-8b-instant",
+    "llama-guard-3-8b",
     "llama3-8b-8192",
     "llama3-70b-8192",
     "mixtral-8x7b-32768",
     "gemma-7b-it",
+    "qwen-qwq-32b",
+    "mistral-saba-24b",
+    "qwen-2.5-32b",
+    "deepseek-r1-distill-qwen-32b",
+    "deepseek-r1-distill-llama-70b-specdec",
+    "deepseek-r1-distill-llama-70b",
+    "llama-3.3-70b-specdec",
 ]
 
 
